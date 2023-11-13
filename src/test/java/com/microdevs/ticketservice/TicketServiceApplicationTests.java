9ddeb72ca1ffe911a94d37bb525f353909c824ee
mert <mk754@sussex.ac.uk>
2023-11-13 17:56:23 +0000

    init commit

---
+++ src/test/java/com/microdevs/ticketservice/TicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microdevs.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
